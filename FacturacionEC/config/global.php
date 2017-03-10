5594e30a110f64327ff5d37ac45c18d67960db1c
Lu-rodriguez <lulyrodriguezv@yahoo.es>
2017-03-10 16:40:25 -0500

    Update global.php

--- FacturacionEC/config/global.php
+++ FacturacionEC/config/global.php
@@ -2,7 +2,7 @@
 /**
  *  constantes que para establecer controladores y acciones por defecto 
  *
- * @author JJ
+ * @author JJ esto esuna prueba
  */
 define("CONTROLADOR_DEFECTO", "Producto");
 define("ACCION_DEFECTO", "index");
